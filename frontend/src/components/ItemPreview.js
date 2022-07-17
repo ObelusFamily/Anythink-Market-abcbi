7b07a87a1ff725e127cfa5793bbf1f2e83f026af
matt <mlock23@gmail.com>
2022-07-17 17:27:37 -0400

    fixes

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image || process.env.PUBLIC_URL + '/placeholder.png' test}
+        src={item.image || process.env.PUBLIC_URL + '/placeholder.png'}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
